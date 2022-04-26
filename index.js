abd4b5c9aee90af14ea64f8ea1be467cb51793ff
Alin <alinjobo@gmail.com>
2022-04-26 18:50:33 +0530

    added computerplay function

--- index.js
+++ index.js
@@ -8,16 +8,19 @@ if(playersel=="rock" && computersel=="scissors")
     return "Rock beats scissors. You win"
 if(playersel=="rock" && computersel=="paper")
     return "Paper beats rock. You lose"
-if(playersel="paper" && computersel=="scissors")
+if(playersel=="paper" && computersel=="scissors")
    return "scissors beat paper you lose"
-if(playersel="paper" && computersel=="rock")
+if(playersel=="paper" && computersel=="rock")
     return "paper beats rock. you win"
-if(playersel="scissors" && computersel="paper")
+if(playersel=="scissors" && computersel=="paper")
 return "scissors beat paper you win"
-if(playersel="scissors" && computersel=="rock")
+if(playersel=="scissors" && computersel=="rock")
 return "rock beats scissors .you lose"
 }
 
 function computerplay(){
-    let selec
+    const selection = ["rock","paper","scissors"]
+    console.log( selection[Math.floor(Math.random()*selection.length)])
+    
 }
+computerplay()
