bd8e528a230e3b0e457c6d5412045a90b6600c96
Alin <alinjobo@gmail.com>
2022-05-07 18:11:59 +0530

    Added Scoring

--- index.js
+++ index.js
@@ -4,31 +4,51 @@ function computerplay(){
     return( selection[Math.floor(Math.random()*selection.length)])
     
 }
+let playerscore=0;
+let computerscore=0;
 
+const result= document.querySelector(".result");
+const pscore=document.querySelector(".score #pscore");
+const cscore=document.querySelector(".score #cscore");
 function round(e){
-   // console.log('a');
+    
     const selection = ["rock","paper","scissors"]
     const computersel=( selection[Math.floor(Math.random()*selection.length)]);
-//console.log(computersel);
-const playersel=e.target.id;
-const result= document.querySelector(".result");
+   // console.log('a');
+   
+    //console.log(computersel);
+    const playersel=e.target.id;
 
-//console.log(playersel);
-if(playersel==computersel)
-    result.textContent= "draw";
-if(playersel=="rock" && computersel=="scissors")
-result.textContent= "Rock beats scissors. You win"
-if(playersel=="rock" && computersel=="paper")
-result.textContent=  "Paper beats rock. You lose"
-if(playersel=="paper" && computersel=="scissors")
-result.textContent=  "scissors beat paper you lose"
-if(playersel=="paper" && computersel=="rock")
-result.textContent=  "paper beats rock. you win"
-if(playersel=="scissors" && computersel=="paper")
-result.textContent=  "scissors beat paper you win"
-if(playersel=="scissors" && computersel=="rock")
-result.textContent=  "rock beats scissors .you lose"
-//result.textContent=  "Enter a valid option"
+    //console.log(playersel);
+    function cwin(){
+        computerscore++;
+        cscore.innerText=computerscore;
+    };
+    function pwin(){
+        playerscore++;
+        pscore.innerText=playerscore;
+    };
+    if(playersel==computersel)
+        result.textContent= "draw";
+    if(playersel=="rock" && computersel=="scissors"){
+        pwin();
+        result.textContent= "Rock beats scissors. You win"}
+    if(playersel=="rock" && computersel=="paper"){
+        cwin();
+        result.textContent=  "Paper beats rock. You lose"}
+    if(playersel=="paper" && computersel=="scissors"){
+        cwin();
+        result.textContent=  "scissors beat paper you lose"}
+    if(playersel=="paper" && computersel=="rock"){
+        pwin();
+        result.textContent=  "paper beats rock. you win"}
+    if(playersel=="scissors" && computersel=="paper"){
+        pwin();
+        result.textContent=  "scissors beat paper you win"}
+    if(playersel=="scissors" && computersel=="rock"){
+        cwin();
+    result.textContent=  "rock beats scissors .you lose"}
+    //result.textContent=  "Enter a valid option"
 }
 
 
